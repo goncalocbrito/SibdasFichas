10f2d07321481e7d64ba704356676f9a0d5c289d
Gonçalo Brito <goncalocbritomd@gmail.com>
2026-03-12 11:27:21 +0000

    Ficha_06 v3.0 - Condições e Preçario dos Produtos e Serviços

--- Ficha_06/isep_ginasio/backend/includes/js/funcoes.js
+++ Ficha_06/isep_ginasio/backend/includes/js/funcoes.js
@@ -42,3 +42,66 @@ function atualizarIndicadorIMC() {
 
 pesoInput.addEventListener("input", atualizarIndicadorIMC);
 alturaInput.addEventListener("input", atualizarIndicadorIMC);
+
+function avaliarCondicoes() {
+    // Ler os estados das checkboxes
+    const problemasCostas = document.getElementById("temProblemasCostas").checked;
+    const gravida = document.getElementById("estaGravida").checked;
+    const diabetico = document.getElementById("temDiabetes").checked;
+    const mensagemElement = document.getElementById("mensagem");
+
+    // Avaliar as combinações e apresentar a recomendação
+    if (gravida) {
+        mensagemElement.textContent = "Encaminhar para o Professor João Faria (Especialista em exercício para grávidas).";
+    } else if (problemasCostas && diabetico) {
+        mensagemElement.textContent = "Necessário plano adaptado. Agendar consulta com especialista.";
+    } else if (problemasCostas || diabetico) {
+        mensagemElement.textContent = "Recomenda-se avaliação personalizada antes de iniciar o treino.";
+    } else {
+        mensagemElement.textContent = "Sem restrições. Pode iniciar o plano de treino geral.";
+    }
+}
+
+function selecionarServico(servico) {
+
+    document.getElementById('opcoesPT').style.display = 'none';
+    document.getElementById('opcoesAG').style.display = 'none';
+
+    document.getElementById('resultadoPT').innerHTML = '';
+    document.getElementById('resultadoAG').innerHTML = '';
+
+    if (servico === 'PT') {
+        document.getElementById('opcoesPT').style.display = 'block';
+    } else {
+        document.getElementById('opcoesAG').style.display = 'block';
+    }
+
+}
+
+function mostrarPrecoPT(vezes) {
+
+    const precos = [54, 103, 150, 198];
+    const preco = precos[vezes - 1];
+
+    document.getElementById('resultadoPT').innerText =
+        `Preço: ${preco} EUR / quinzena`;
+
+}
+
+function mostrarPrecoAG(freq) {
+
+    let preco = '';
+
+    if (freq == 1) {
+        preco = '16.9 EUR / quinzena';
+
+    } else if (freq == 2) {
+        preco = '26 EUR / quinzena';
+
+    } else {
+        preco = '31.9 EUR / quinzena';
+    }
+
+    document.getElementById('resultadoAG').innerText = 'Preço: ' + preco;
+
+}

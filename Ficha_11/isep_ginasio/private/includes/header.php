0890428115652a03fcd8c692355e0c77946f5818
Gonçalo Brito <goncalocbritomd@gmail.com>
2026-03-18 17:16:31 +0000

    Ficha_11 v3.0 - Filtro e Pesquisa da tabela clientes

--- Ficha_11/isep_ginasio/private/includes/header.php
+++ Ficha_11/isep_ginasio/private/includes/header.php
@@ -26,6 +26,12 @@ require_once __DIR__ . '/../../config/config.php';
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/SIBDAS/Ficha_11/isep_ginasio/private/assets/fontawesome/all.min.css">
 
+    <!-- jQuery -->
+    <script src="/SIBDAS/Ficha_11/isep_ginasio/private/assets/jquery/jquery-3.6.0.min.js"></script>
+    
+    <!-- DataTables CSS + JS -->
+    <link rel="stylesheet" href="/SIBDAS/Ficha_11/isep_ginasio/private/assets/datatables/datatables.min.css">
+    <script src="/SIBDAS/Ficha_11/isep_ginasio/private/assets/datatables/datatables.min.js"></script>
 </head>
 
 <body>

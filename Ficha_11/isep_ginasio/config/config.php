5905d010fe5abc41d304de1c5c98b1dab1381f79
Gonçalo Brito <goncalocbritomd@gmail.com>
2026-03-18 12:56:12 +0000

    Ficha_11 v1.0 - Sessões em Views

---
+++ Ficha_11/isep_ginasio/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('BASE_URL', '/isep-ginasio');
+
+// Configurações globais da aplicação
+define('APP_NAME', 'ISEP Porto Ginásio');
+define('APP_VERSION', '1.0.0');
+define('APP_COPYRIGHT', '© 2025 ISEP');
---
+++ Ficha_11/isep_ginasio/private/includes/footer.php
@@ -0,0 +1,4 @@
+    <!-- Bootstrap JS and custom JS -->
+    <script src="/SIBDAS/Ficha_11/isep_ginasio/private/assets/bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+</html>
